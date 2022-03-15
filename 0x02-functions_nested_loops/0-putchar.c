9375aa488ce8b74cbe70d27ab328efef430b4ddc
bobkamau <Robertmwangi369@gmail.com>
2022-03-15 20:55:20 +0300

    task 0 code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/**
- *main - Entry point
- *Return:  0
- */
 
+/**
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	_putchar('_');
