d3cc1872f060cba6256735cd8c2fca4fc6b2d9bc
bobkamau <robertmwangi369@gmail.com>
2022-03-21 00:18:01 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strien.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strien - function that returns the length of a string
+ * @s: character
+ * Return: Always 0.
+ */
+
+int _strien(char *s)
+{
+	int i;
+
+	for (i=0; s[i] != '\0'; i++)
+	{
+		i = i;
+	}
+	return (i);
+}
+
