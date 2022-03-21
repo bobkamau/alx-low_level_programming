a3835f7b74e1a0eef7d0cd8a3cfb43e613714fb8
bobkamau <robertmwangi369@gmail.com>
2022-03-21 02:14:40 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * puts_half - function that prints half of a string
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
