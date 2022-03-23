abb10c9f262c0db5fbb15668019aeaf11f0b5f71
bobkamau <Robertmwangi369@gmail.com>
2022-03-23 11:17:28 +0300

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,29 @@
 #include "main.h"
 
 /**
- *  * *rot13 - encodes a string using rot13
- *   * @s: string
- *    * Return: encoded string
- *     **/
+ * *rot13 - encodes a string using rot13
+ * @s: string
+ * Return: encoded string
+ **/
 
 char *rot13(char *s)
 {
-		int i;
+	int i;
 
-			i = 0;
+	i = 0;
 
-				while (s[i])
-						{
-									while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-												{
-																if ((s[i] >= 'a' && s[i] < 'n') || (s[i] >= 'A' && s[i] < 'N'))
-																				{
-																									s[i] = s[i] + 26;
-																												}
-																			s[i] = s[i] - 13;
-																					break;
-																							}
-											i++;
-												}
-					return (s);
+	while (s[i])
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] < 'n') || (s[i] >= 'A' && s[i] < 'N'))
+			{
+				s[i] = s[i] + 26;
+			}
+			s[i] = s[i] - 13;
+		break;
+		}
+		i++;
+	}
+	return (s);
 }
-
