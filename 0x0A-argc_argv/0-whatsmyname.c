18a41ccfe318d447ac33c3a6e47eb71ce2f27598
bobkamau <Robertmwangi369@gmail.com>
2022-04-01 20:32:37 +0300

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,21 +1,20 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- *  *main - prints the program name
- *   *@argc: captures the number of commands in the command line
- *    *@argv: captures the strings in the command line
- *     *Return: always 0
- *      */
+ *main - prints the program name
+ *@argc: captures the number of commands in the command line
+ *@argv: captures the strings in the command line
+ *Return: always 0
+ */
 
 int main(int argc, char *argv[])
 {
-		int i;
+	int i;
 
-			for (i = 0; i < argc; i++)
-					{
-								printf("%s\n", argv[i]);
-									}
-				return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
-
