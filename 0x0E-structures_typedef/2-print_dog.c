8c44b32e20129e6855e8ed7dc30d91c31cd19420
bobkamau <Robertmwangi369@gmail.com>
2022-04-11 17:57:23 +0300

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,19 +3,18 @@
 #include "dog.h"
 
 /**
- *  * print_dog - Prints a struct dog
- *   * @d: pointer to struct dog
- *    */
+ * print_dog - Prints a struct dog
+ * @d: pointer to struct dog
+ */
 
 void print_dog(struct dog *d)
 {
-		if (d != NULL)
-				{
-							printf("Name: ");
-									d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
-											printf("Age: %f\n", d->age);
-													printf("Owner: ");
-															d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
-																}
+	if (d != NULL)
+	{
+		printf("Name: ");
+		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: ");
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
+	}
 }
-
