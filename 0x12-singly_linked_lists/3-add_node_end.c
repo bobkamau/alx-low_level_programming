7dd6f1bb4314c67414fd385a04422e734bf33cb9
bobkamau <Robertmwangi369@gmail.com>
2022-04-21 18:53:55 +0300

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,59 +3,58 @@
 #include <string.h>
 
 /**
- * * _strlen - finds the length of a string
- * * @str: string to find the length of
- * * Return: length of string
- * */
+* _strlen - finds the length of a string
+* @str: string to find the length of
+* Return: length of string
+*/
 
 
 unsigned int _strlen(char *str)
 {
-		unsigned int i;
+	unsigned int i;
 
-			for (i = 0; str[i]; i++)
-						;
-				return (i);
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
 }
 
 /**
- * * add_node_end - add new node to end of linked list
- * * @head: double pointr to linked list
- * * @str: string to add
- * * Return: pointer to new node
- * */
+* add_node_end - add new node to end of linked list
+* @head: double pointr to linked list
+* @str: string to add
+* Return: pointer to new node
+*/
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-		list_t *new, *tmp;
+	list_t *new, *tmp;
 
-			if (str == NULL)
-						return (NULL);
-				new = malloc(sizeof(list_t));
-					if (new == NULL)
-								return (NULL);
-						new->str = strdup(str);
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
 
-							if (new->str == NULL)
-									{
-												free(new);
-														return (NULL);
-															}
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-								new->len = _strlen(new->str);
-									new->next = NULL;
+	new->len = _strlen(new->str);
+	new->next = NULL;
 
-										if (*head == NULL)
-												{
-															*head = new;
-																	return (new);
-																		}
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 
-											tmp = *head;
-												while (tmp->next)
-															tmp = tmp->next;
-													tmp->next = new;
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
 
-														return (new);
+	return (new);
 }
-
